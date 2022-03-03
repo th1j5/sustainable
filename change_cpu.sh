75c7a3b218557008aa4f5dc4e7c01257a1f0e820
mahmoodn <nt_mahmood@yahoo.com>
2022-03-03 23:38:40 +0100

    commit

--- change_cpu.sh
+++ change_cpu.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-# Argument is 'powersave' or 'performance'
+# Argument is 'powersave' or 'performance' or 'ondemand'
+# Run `cpupower frequency-info` to see the list
 sudo cpupower frequency-set --governor $1
