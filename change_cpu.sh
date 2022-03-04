4d1ff9268d3a480d2e8447a8c9e30a9da6a0a654
mahmoodn <nt_mahmood@yahoo.com>
2022-03-04 13:28:10 +0100

    commit

--- change_cpu.sh
+++ change_cpu.sh
@@ -2,4 +2,15 @@
 
 # Argument is 'powersave' or 'performance' or 'ondemand'
 # Run `cpupower frequency-info` to see the list
-sudo cpupower frequency-set --governor $1
+MIN_FREQ=`/sys/devices/system/cpu/cpufreq/policy0/scaling_min_freq`
+MAX_FREQ=`/sys/devices/system/cpu/cpufreq/policy0/scaling_max_freq`
+if [ $1 -eq "powersave" ]; then
+  echo "Minimum frequency is $MIN_FREQ Hz"
+  #sudo cpupower frequency-set -u $MIN_FREQ
+elif [ $1 -eq "performance" ]; then
+  echo "Maximum frequency is $MAX_FREQ Hz"
+  #sudo cpupower frequency-set -d $Max_FREQ
+else
+  echo "Wrong argument. Use powersave or performance"
+fi
+
