ab2a09871e6e1df85025620afc625d3a973e3281
mahmoodn <nt_mahmood@yahoo.com>
2022-03-04 13:42:20 +0100

    commit

--- change_cpu.sh
+++ change_cpu.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-# Argument is 'powersave' or 'performance' or 'ondemand'
-# Run `cpupower frequency-info` to see the list
-MIN_FREQ=`/sys/devices/system/cpu/cpufreq/policy0/scaling_min_freq`
-MAX_FREQ=`/sys/devices/system/cpu/cpufreq/policy0/scaling_max_freq`
-if [ $1 -eq "powersave" ]; then
+# Run `cpupower frequency-info` to see information about policies
+MIN_FREQ=`sudo cat /sys/devices/system/cpu/cpufreq/policy0/cpuinfo_min_freq`
+MAX_FREQ=`sudo cat /sys/devices/system/cpu/cpufreq/policy0/cpuinfo_max_freq`
+if [ $1 = "powersave" ]; then
   echo "Minimum frequency is $MIN_FREQ Hz"
-  #sudo cpupower frequency-set -u $MIN_FREQ
-elif [ $1 -eq "performance" ]; then
+  sudo cpupower frequency-set -d $MIN_FREQ
+  sudo cpupower frequency-set -u $MIN_FREQ
+elif [ $1 = "performance" ]; then
   echo "Maximum frequency is $MAX_FREQ Hz"
-  #sudo cpupower frequency-set -d $Max_FREQ
+  sudo cpupower frequency-set -d $MAX_FREQ
+  sudo cpupower frequency-set -u $MAX_FREQ
+elif [ $1 = "balance" ]; then
+  echo "Frequency range is $MIN_FREQ - $MAX_FREQ"
+  sudo cpupower frequency-set -d $MIN_FREQ
+  sudo cpupower frequency-set -u $MAX_FREQ
 else
-  echo "Wrong argument. Use powersave or performance"
+  echo "Wrong argument. Use powersave or performance or balance"
 fi
+
 
