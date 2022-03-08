f80fa4f5e270dc5992648d1dac48a6cdbd836fa4
Mahmood Naderan <nt_mahmood@yahoo.com>
2022-03-08 09:58:45 +0100

    Update build_c.sh

--- build_c.sh
+++ build_c.sh
@@ -4,5 +4,6 @@ gcc -O3 -fsplit-stack -o mm_float mm_float.c
 gcc -O3 -fsplit-stack -o mm_double mm_double.c
 gcc -O3 -fsplit-stack -fopenmp -o mm_double_omp mm_double_omp.c
 
+# Do not forget to install the libopenblas-dev package
 gcc -O3 -fsplit-stack -fopenmp -c mm_double_blas.c -o mm_double_blas.o
 g++ -std=c++11 -o  mm_double_blas mm_double_blas.o -lblas -fopenmp
